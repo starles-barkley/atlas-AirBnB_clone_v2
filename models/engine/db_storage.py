0be3feaf74fe8ae2d7fbb4896d35e88824f52044
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-02-25 11:03:59 +0000

    add the reload, delete, save, and new methods back to the db_storage and modify init method to include env variables

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,26 +1,70 @@
 #!/usr/bin/python3
-"""This module defines a class DBStorage for the storage engine"""
-from sqlalchemy import create_engine, MetaData
-from sqlalchemy.orm import sessionmaker
-from models.base_model import Base
-from os import environ as env
 
+"""This module defines a class for creating the engine"""
+import os
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+
+
+Base = declarative_base()
 
 class DBStorage:
-    """This class defines the MySQL database storage engine."""
+    """This class manages storage of hbnb models in database format"""
     __engine = None
     __session = None
 
     def __init__(self):
-        """Creates an instance of DBStorage."""
+        """Initialize engine for db_storage"""
+
+        user = os.getenv('HBNB_MYSQL_USER', 'hbnb_dev')
+        pwd = os.getenv('HBNB_MYSQL_PWD', 'hbnb_dev_db')
+        host = os.getenv('HBNB_MYSQL_HOST', 'localhost')
+        db = os.getenv('HBNB_MYSQL_DB', 'hbnb_dev_db')
+        env = os.getenv('HBNB_ENV')
+
+        connect = "mysql+mysqldb://{}:{}@{}/{}".format(user, pwd, host, db)
+
         self.__engine = create_engine(connect, pool_pre_ping=True)
-        self.__session = sessionmaker(bind=self.__engine)
-        if environment == 'test':
-            db_metadata.drop_all()
+
+        if env == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
+        
 
     def all(self, cls=None):
-        res = {}
+        """Implement query and return as dictionary"""
         if cls is None:
-            query = self.__session.query().all()
+            result = {}
+            for model in [User, State, City, Amenity, Place, Review]:
+                for instance in self.__session.query(model).all():
+                    key = '{}.{}'.format(model.__name__, instance.id)
+                    result[key] = instance
+            return result
         else:
-            query = self.__session.query(cls).all()
+            return {f"{cls.__name__}.{instance.id}": instance for instance in self.__session.query(cls).all()}
+
+    def new(self, obj):
+        """Add object to session"""
+        self.__session.add(obj)
+
+    def save(self):
+        """Commit session changes"""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """Delete object from session"""
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """Recreate tables and create new session"""
+        Base.metadata.create_all(self.__engine)
+        self.__session.close()
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
