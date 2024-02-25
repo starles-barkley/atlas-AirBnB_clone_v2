bbc7437f374593461ae94dfa5d26c527107130a9
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-02-25 07:29:31 +0000

    scrap unittest for User class and use mine from previous console project but revise to include more documentation

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,28 +1,20 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+import unittest
+from models.state import State
 from models.user import User
 
+class TestUser(unittest.TestCase):
 
-class TestUser(test_basemodel):
-    """ Tests the user class """
+    def setUp(self):
+        """ Sets up a User for testing """
+        self.my_user = User()
 
-    def test_is_first_name_a_string(self):
-        """ Tests if the type of first_name is a string """
-        new = self.value()
-        self.assertEqual(type(new.first_name), str)
+    def test_attributes_set_to_strings(self):
+        """ Checks if User attributes initialized as strings """
+        self.assertEqual(self.my_user.email, "")
+        self.assertEqual(self.my_user.password, "")
+        self.assertEqual(self.my_user.first_name, "")
+        self.assertEqual(self.my_user.last_name, "")
 
-    def test_is_last_name_a_string(self):
-        """ Checks if type of last_name is a string """
-        new = self.value()
-        self.assertEqual(type(new.last_name), str)
-
-    def test_is_email_a_string(self):
-        """ Checks if type of email is a string """
-        new = self.value()
-        self.assertEqual(type(new.email), str)
-
-    def test_is_password_a_string(self):
-        """ Checks if type of password is a string """
-        new = self.value()
-        self.assertEqual(type(new.password), str)
+if __name__ == '__main__':
+    unittest.main()
