17e3a1137500ac4d62effb2f5035e90737b0fb1c
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-02-25 12:14:35 +0000

    change state module back to working code

--- models/state.py
+++ models/state.py
@@ -1,22 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
 
-class State(BaseModel, Base):
+
+class State(BaseModel):
     """ State class """
-    __tablename__ = 'states'
-
-    name = Column(String(128), nullable=False)
-    
-    # For DBStorage
-    cities = relationship("City", backref="state", cascade="all, delete-orphan")
-
-    # For FileStorage
-    @property
-    def cities(self):
-        """Getter attribute for cities in FileStorage"""
-        from models import storage
-        all_cities = storage.all("City")
-        return [city for city in all_cities.values() if city.state_id == self.id]
+    name = ""
