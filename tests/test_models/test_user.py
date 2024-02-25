181b8a13ea1b73b196cc037e98e3887e7b3652a1
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-02-25 07:12:28 +0000

    revise the test_user file to include documentation, change the test class to CamelCasing

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -4,31 +4,31 @@ from tests.test_models.test_base_model import test_basemodel
 from models.user import User
 
 
-class test_User(test_basemodel):
-    """ """
+class TestUser(test_basemodel):
+    """ Tests the user class """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """  """
         super().__init__(*args, **kwargs)
         self.name = "User"
         self.value = User
 
     def test_first_name(self):
-        """ """
+        """ Tests if the type of first_name is a string """
         new = self.value()
         self.assertEqual(type(new.first_name), str)
 
     def test_last_name(self):
-        """ """
+        """ Checks if type of last_name is a string """
         new = self.value()
         self.assertEqual(type(new.last_name), str)
 
     def test_email(self):
-        """ """
+        """ Checks if type of email is a string """
         new = self.value()
         self.assertEqual(type(new.email), str)
 
     def test_password(self):
-        """ """
+        """ Checks if type of password is a string """
         new = self.value()
         self.assertEqual(type(new.password), str)
