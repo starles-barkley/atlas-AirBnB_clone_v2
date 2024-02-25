438df64a4b055005211e71d2ac8194fd85a0f13d
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-02-25 08:10:30 +0000

    attempt to create mysql test dev script with tests to create the db and user

---
+++ setup_mysql_test.sql
@@ -0,0 +1,5 @@
+-- Create or use existing database hbnb_test_db
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create or use existing user hbnb_test
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_pwd';
