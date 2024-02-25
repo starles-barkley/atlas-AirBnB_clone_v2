95a314c9a72bc6697226b971cbea00ceb74bc485
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-02-25 12:09:39 +0000

    change init.py models package back to original

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,6 @@
-#!/usr/bin/python3
-"""Initialize the models package"""
-import os
+"""This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
 
-storage_type = os.environ.get('HBNB_TYPE_STORAGE', 'file')
 
-if storage_type == 'db':
-    from models.engine.db_storage import DBStorage
-    storage = DBStorage()
-else:
-    storage = FileStorage()
-
+storage = FileStorage()
 storage.reload()
