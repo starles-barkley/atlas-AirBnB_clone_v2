cd20d9e5e2d7c6b0086dde5d9052cebb9ee52d7f
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-02-25 10:37:50 +0000

    fix indentation errors in base model file

--- models/base_model.py
+++ models/base_model.py
@@ -9,12 +9,13 @@ import models
 
 Base = declarative_base()
 
-class BaseModel:
+class BaseModel(Base):
     """A base class for all hbnb models"""
     id = Column(String(60), nullable=False, primary_key=True)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-     def __init__(self, *args, **kwargs):
+
+    def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
         if not kwargs:
             self.id = str(uuid.uuid4())
@@ -28,17 +29,20 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != '__class__':
                     setattr(self, key, value)
+
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+
     def save(self):
-        """Updates updated_at with current time when instance is changed"""
+        """Updates updated_at with the current time when the instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
-        
+
         storage.new(self)
         storage.save()
+
     def to_dict(self):
         """Converts instance into dict format"""
         dictionary = self.__dict__.copy()
