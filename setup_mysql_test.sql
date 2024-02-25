fee8600bdf77c4cfdf0fc42e966bc358dc12ae05
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-02-25 08:14:27 +0000

    add commands to sql test script to grant all permissions to user

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,3 +3,9 @@ CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
 -- Create or use existing user hbnb_test
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_pwd';
+
+-- Grant all permissions for user
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
