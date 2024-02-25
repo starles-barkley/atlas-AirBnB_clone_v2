2e40be392f56fb4799f329b7d48ca383116bea6d
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-02-25 08:16:26 +0000

    add additional sql command to grant reading permission on performance schema

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -7,5 +7,8 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_pwd';
 -- Grant all permissions for user
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
+-- Grant SELECT privilege on performance_schema to hbnb_test
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
 -- Flush privileges to apply changes
 FLUSH PRIVILEGES;
