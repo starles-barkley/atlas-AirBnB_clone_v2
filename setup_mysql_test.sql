ca2f191cbcf46f0c6ce8c7b9d5dd05304456152d
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-02-25 08:19:14 +0000

    fix typo in sql test script

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,9 +2,9 @@
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
 -- Create or use existing user hbnb_test
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
--- Grant all permissions for user
+-- Grant all privileges on hbnb_test_db to hbnb_test
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
 -- Grant SELECT privilege on performance_schema to hbnb_test
