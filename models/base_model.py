0fc49bedc1c49df00cd7ca14a1c14b17d806fccf
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-02-26 23:35:23 +0000

    remove whitespaces from BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -21,18 +21,18 @@ class BaseModel:
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
             self.__dict__.update(kwargs)
-    
+
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
-    
+
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
         storage.save()
-    
+
     def to_dict(self):
         """Convert instance into dict format"""
         dictionary = {}
