69a47206baee2e1f1e2729d7d777b47a0b1936a3
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-02-26 23:37:06 +0000

    shorten line length to satisfy pycodestyle requirements

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,7 +13,8 @@ class FileStorage:
         if cls is None:
             return FileStorage.__objects
         else:
-            return {k: v for k, v in FileStorage.__objects.items() if isinstance(v, cls)}
+            return {k: v for k, v in 
+                    FileStorage.__objects.items() if isinstance(v, cls)}
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
