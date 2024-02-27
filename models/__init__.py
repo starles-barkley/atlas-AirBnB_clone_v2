760782ee9132020667cff713fa64b2f19a7691fd
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-02-27 21:20:23 +0000

    pull code from my models.__init__ file in my other branch

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,15 @@
-"""This module instantiates an object of class FileStorage"""
+#!/usr/bin/python3
+"""Initialize the models package"""
+import os
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
 
+storage_type = os.environ.get('HBNB_TYPE_STORAGE', 'file')
 
-storage = FileStorage()
+if storage_type == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    storage = FileStorage()
+
 storage.reload()
