32021bcce4696e47b37877273fb4f3a9d0e79d9f
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-02-28 00:25:14 +0000

    fix basemodel?

--- models/base_model.py
+++ models/base_model.py
@@ -1,50 +1,57 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
-import uuid
+from uuid import uuid4
 from datetime import datetime
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 import models
-import os
-storage_type = os.environ.get('HBNB_TYPE_STORAGE', 'file')
-
+import time
 
 Base = declarative_base()
+
 
 class BaseModel:
     """A base class for all hbnb models"""
 
-    id = Column('id', String(60), primary_key=True)
-    created_at = Column('created_at', DateTime, nullable=False, default=datetime.utcnow())
-    updated_at = Column('updated_at', DateTime, nullable=False, default=datetime.utcnow())
+    # Define SQLAlchemy columns for id, created_at, and updated_at
+    id = Column(String(60), nullable=False, primary_key=True, unique=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        # from models.engine.file_storage import storage
+        """Instantiation of BaseModel object"""
 
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+        # Set id attribute to a new UUID if not provided
+        self.id = kwargs.get('id', str(uuid4()))
+
+        # Set created_at attribute to current datetime if not provided
+        self.created_at = kwargs.get('created_at', datetime.now())
+
+        # Set updated_at attribute to current datetime or provided value
+        self.updated_at = kwargs.get('updated_at', self.created_at)
+
+        # Set additional attributes from kwargs dynamically
+        for key, value in kwargs.items():
+            if key not in ["id", "created_at", "updated_at", "__class__"]:
+                setattr(self, key, value)
 
     def __str__(self):
-        """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        """String representation of BaseModel object"""
+        return '[{}] ({}) {}'.format(
+            type(self).__name__, self.id, self.__dict__
+        )
+
+    def delete(self):
+        """Delete current instance from FileStorage"""
+        from models import storage
+        storage.delete(self)
 
     def save(self):
-        """Updates updated_at with current time when instance is changed"""
-        # from models.engine.file_storage import storage
+        """Update updated_at with current datetime and save to storage"""
+        from models import storage
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.new(self)
+        storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
@@ -54,11 +61,6 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        if '_sa_instance_state' in dictionary:
-            del dictionary['_sa_instance']
+        if '_sa_instance_state' in dictionary.keys():
+            dictionary.pop('_sa_instance_state')
         return dictionary
-
-    def delete(self):
-        """Deletes current instance from storage"""
-        # from models.engine.file_storage import storage
-        models.storage.delete(self)
