aa03aa73988e3d5350b4073a0b14fd8e9b10208c
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-02-28 18:23:20 +0000

    pull code for state from other branch

--- models/state.py
+++ models/state.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+    
+    # For DBStorage
+    cities = relationship("City", backref="state", cascade="all, delete-orphan")
+
+    # For FileStorage
+    @property
+    def cities(self):
+        """Getter attribute for cities in FileStorage"""
+        from models import storage
+        all_cities = storage.all("City")
+        return [city for city in all_cities.values() if city.state_id == self.id]
