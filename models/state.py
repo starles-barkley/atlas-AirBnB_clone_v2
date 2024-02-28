7389e2b11a751959c0e31cbef75cd05d1a10fffd
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-02-28 00:38:50 +0000

    try adding state file back into mix

--- models/state.py
+++ models/state.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, base
+from sqlalchemy import Table, Column, Integer, String, ForeignKey
 
-
-class State(BaseModel):
+class State(BaseModel, base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
