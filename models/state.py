0458216a4eec373b8f8984a1c3381a3f2b2f2f7d
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-02-28 17:59:54 +0000

    change state file back

--- models/state.py
+++ models/state.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+a
 
-class State(BaseModel, Base):
+class State(BaseModel):
     """ State class """
-    __tablename__ = 'states'
-
-    name = Column(String(128), nullable=False)
+    name = ""
