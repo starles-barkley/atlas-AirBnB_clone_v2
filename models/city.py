e356a972ad74e368443be435329b7403050740ca
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-02-28 00:37:33 +0000

    try easing the city file back into the mix

--- models/city.py
+++ models/city.py
@@ -1,7 +1,13 @@
+#!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, base
 
-
-class City(BaseModel):
+class City(BaseModel, base): 
     """ The city class, contains state ID and name """
-    state_id = ""
+    __tablename__ = 'cities'
+    id = Column(String(60), primary_key=True, nullable=False) 
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", backref="city", cascade="all, delete")
