75d966641acaa507b171d9957335d5b0fef4855f
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-01 03:17:41 +0000

    remove test for name in city test

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -10,15 +10,9 @@ class test_City(test_basemodel):
     def __init__(self, *args, **kwargs):
         """ """
         super().__init__(*args, **kwargs)
-        self.name = "City"
         self.value = City
 
     def test_state_id(self):
         """ """
         new = self.value()
         self.assertEqual(type(new.state_id), str)
-
-    def test_name(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
