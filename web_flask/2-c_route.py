a591074160b6300183ac38d3f0efc3d6d406683d
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-08 02:26:38 +0000

    create a new flask app with 3 routes

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''This module starts a Flask web app'''
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_text(text):
+    # Replace underscores with spaces in the text variable
+    formatted_text = text.replace('_', ' ')
+    return f'C {formatted_text}'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
