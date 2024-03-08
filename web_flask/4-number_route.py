bbd473b56606f6bb7d0327a3052a20ca2192fd77
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-08 20:38:30 +0000

    add int to num_route path

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -29,7 +29,7 @@ def display_python(text='is_cool'):
     return f'Python {formatted_text}'
 
 
-@app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def num_route(n):
     return f'{n} is a number'
 
