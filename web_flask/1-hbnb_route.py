37e027829b2c1cd5ef15bb1905994d16e94a9dcd
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-08 02:22:38 +0000

    fix blank line mistakes

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,16 +7,14 @@ app = Flask(__name__)
 
 
 app.route('/', strict_slashes=False)
-
-
 def hello_hbnb():
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
-
-
 def display_hbnb():
     return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
