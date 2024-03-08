342f353135d7ffa186701c8297b0b44ab7665018
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-08 02:21:26 +0000

    add extra lines for pycodestyle

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,10 +7,14 @@ app = Flask(__name__)
 
 
 app.route('/', strict_slashes=False)
+
+
 def hello_hbnb():
     return 'Hello HBNB!'
 
 @app.route('/hbnb', strict_slashes=False)
+
+
 def display_hbnb():
     return 'HBNB'
 
