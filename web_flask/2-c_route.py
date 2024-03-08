fb59b28596121027ea9ff102645209cf4599b089
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-08 02:28:39 +0000

    fix new lines

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -5,17 +5,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def display_hbnb():
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def display_text(text):
-    # Replace underscores with spaces in the text variable
     formatted_text = text.replace('_', ' ')
     return f'C {formatted_text}'
 
