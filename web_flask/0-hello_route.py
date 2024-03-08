92604f870ce3b68f1383f237a98090255840a5d7
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-08 01:58:41 +0000

    add documentation to route handler

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """
+    Route handler for the root URL (/).
+    
+    """
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
