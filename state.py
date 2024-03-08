855d04ee9b1e13b6e57b7f7326a461d331a6cdb3
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-07 19:11:26 -0600

    add files for models folder

---
+++ state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" holds class State"""
+import models
+from models.base_model import BaseModel, Base
+from models.city import City
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class State(BaseModel, Base):
+    """Representation of state """
+    if models.storage_t == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
+	cites = []
