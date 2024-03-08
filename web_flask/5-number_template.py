54110c8ddeb01524c2f5d89faec4a22d5290a607
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-08 20:54:01 +0000

    update number_template?

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -39,5 +39,12 @@ def num_template(n):
     return f'Number {n}'
 
 
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def display_number_template(n):
+    if isinstance(n, int):
+        return render_template('number_template.html', number=n)
+    else:
+        return 'Not a valid integer'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
