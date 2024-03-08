8507b6cc045463d8600781c4e4758289d0029f84
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-08 02:31:50 +0000

    create script that adds a fourth route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''This module starts a Flask web app'''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    formatted_text = text.replace('_', ' ')
+    return f'C {formatted_text}'
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def display_python(text='is_cool'):
+    formatted_text = text.replace('_', ' ')
+    return f'Python {formatted_text}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
