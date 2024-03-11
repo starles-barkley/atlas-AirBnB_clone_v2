8abbad2e14690efc5aa3670c6f3e3baa498ba569
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-11 20:44:44 +0000

    fix my documentation

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""
-Module containing flask script with api route that displays 'HBNB'
-"""
+'''This module starts a Flask web app'''
+
 from flask import Flask
 from flask import render_template
 
@@ -9,45 +8,35 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
+def hello_hbnb():
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
+def display_hbnb():
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_route(text):
-    new_str = ""
-    for i in text:
-        if i == "_":
-            new_str = new_str + " "
-        else:
-            new_str = new_str + i
-    return 'C {}'.format(new_str)
+def display_c(text):
+    formatted_text = text.replace('_', ' ')
+    return f'C {formatted_text}'
 
 
-@app.route('/python', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_route(text="is cool"):
-    new_str = ""
-    for i in text:
-        if i == "_":
-            new_str = new_str + " "
-        else:
-            new_str = new_str + i
-    return "Python {}".format(new_str)
+def display_python(text='is_cool'):
+    formatted_text = text.replace('_', ' ')
+    return f'Python {formatted_text}'
 
 
-@app.route("/number/<int:n>", strict_slashes=False)
-def number_route(n):
-    return "{} is a number".format(n)
+@app.route('/number/<int:n>', strict_slashes=False)
+def num_route(n):
+    return f'{n} is a number'
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def number_template(n):
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def num_template(n: int):
     return render_template('5-number.html', n=n)
 
 
