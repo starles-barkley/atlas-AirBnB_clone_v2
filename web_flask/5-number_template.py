d09b2c1be23929914300dbaddbda54e7aa287f25
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-11 20:41:25 +0000

    import render module

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,41 +1,53 @@
 #!/usr/bin/python3
-'''This module starts a Flask web app'''
-
+"""
+Module containing flask script with api route that displays 'HBNB'
+"""
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
+def hello():
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def display_hbnb():
+def hbnb():
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def display_c(text):
-    formatted_text = text.replace('_', ' ')
-    return f'C {formatted_text}'
+def c_route(text):
+    new_str = ""
+    for i in text:
+        if i == "_":
+            new_str = new_str + " "
+        else:
+            new_str = new_str + i
+    return 'C {}'.format(new_str)
 
 
-@app.route('/python/', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def display_python(text='is_cool'):
-    formatted_text = text.replace('_', ' ')
-    return f'Python {formatted_text}'
+def python_route(text="is cool"):
+    new_str = ""
+    for i in text:
+        if i == "_":
+            new_str = new_str + " "
+        else:
+            new_str = new_str + i
+    return "Python {}".format(new_str)
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def num_route(n):
-    return f'{n} is a number'
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_route(n):
+    return "{} is a number".format(n)
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def num_template(n: int):
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
     return render_template('5-number.html', n=n)
 
 
