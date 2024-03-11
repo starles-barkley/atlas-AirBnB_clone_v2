e8ef79a5ef5c8b3bf635ca744940d68af8bde4b4
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-11 20:48:22 +0000

    copy code from task 5 and put in task 6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+'''This module starts a Flask web app'''
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    formatted_text = text.replace('_', ' ')
+    return f'C {formatted_text}'
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_python(text='is_cool'):
+    formatted_text = text.replace('_', ' ')
+    return f'Python {formatted_text}'
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def num_route(n):
+    return f'{n} is a number'
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def num_template(n: int):
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
