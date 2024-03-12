a24983382086b55a17896846113ff2fa74823e77
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-12 18:27:28 +0000

    test to see if altering city state route works

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,24 +1,33 @@
 #!/usr/bin/python3
 '''This starts an application'''
-
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-from models.city import City
 
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown_appcontext(exception):
-    storage.close()
-
-
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda x: x.name)
-    return render_template('7-cities_by_states.html', states=sorted_states)
+    """
+    Display a list of states and their associated cities
+    """
+    states_dict = storage.all(State)
+    states = {state_id: state for state_id, state in states_dict.items()}
+    # Sort states by name
+    sorted_states = sorted(states.values(), key=lambda x: x.name)
+    # Sort cities within each state by name
+    for state in sorted_states:
+        state.cities.sort(key=lambda x: x.name)
+    return render_template('8-cities_by_states.html', states=sorted_states)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """
+    Removes the current SQLAlchemy Session.
+    """
+    storage.close()
 
 
 if __name__ == '__main__':
