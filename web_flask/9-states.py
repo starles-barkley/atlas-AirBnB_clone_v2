7e0c93c0d865944bbd559935db35a0da5f34db07
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-12 20:12:19 +0000

    add new routes to 9-states

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,35 +1,89 @@
 #!/usr/bin/python3
-''' This module starts an application'''
+'''This module starts a Flask web app'''
 
-from flask import Flask, render_template
-from models import storage
-from models.state import State
-from models.city import City
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown_appcontext(exception):
-    storage.close()
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    formatted_text = text.replace('_', ' ')
+    return f'C {formatted_text}'
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_python(text='is_cool'):
+    formatted_text = text.replace('_', ' ')
+    return f'Python {formatted_text}'
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def num_route(n):
+    return f'{n} is a number'
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def num_template(n: int):
+    return render_template('5-number.html', n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def odd_or_even(n):
+    oddness = ""
+    if n % 2 == 0:
+        oddness = "even"
+    else:
+        oddness = "odd"
+    return render_template("6-number_odd_or_even.html", n=n, oddness=oddness)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda x: x.name)
+    return render_template('7-states_list.html', states=sorted_states)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    states_dict = storage.all(State)
+    states = {state_id: state for state_id, state in states_dict.items()}
+    sorted_states = sorted(states.values(), key=lambda x: x.name)
+    for state in sorted_states:
+        state.cities.sort(key=lambda x: x.name)
+    return render_template('8-cities_by_states.html', states=sorted_states)
 
 
 @app.route('/states', strict_slashes=False)
-def states():
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda x: x.name)
-    return render_template('9-states.html', states=sorted_states)
+def state():
+    states = storage.all(State)
+    return render_template('9-states.html', states=states, mode='all')
 
 
 @app.route('/states/<id>', strict_slashes=False)
-def states_by_id(id):
-    state = storage.get(State, id)
-    if state:
-        cities = sorted(state.cities, key=lambda x: x.name)
-        return render_template('9-states_by_id.html', state=state, cities=cities)
-    else:
-        return render_template('9-not_found.html')
+def state_id(id):
+    for state in storage.all(State).values():
+        if state.id == id:
+            return render_template('9-states.html', states=state, mode='id')
+    return render_template('9-states.html', states=state, mode='none')
 
+
+@app.teardown_appcontext
+def teardown(exception):
+    storage.close()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
