70ca53a5755b6d1e700918077a21de64b5171581
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-13 02:43:59 +0000

    try modifying the models module

--- models/__init__.py
+++ models/__init__.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""
-initialize the models package
-"""
-
+"""This module instantiates an object of class FileStorage"""
+from os import environ as env
 from os import getenv
 
-storage_t = getenv("HBNB_TYPE_STORAGE")
 
-if storage_t == "db":
+env = getenv('HBNB_TYPE_STORAGE')
+if env == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
+    storage.reload()
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-storage.reload()
+    storage.reload()
