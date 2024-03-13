8c3116405994c554acb57ce7ed777c2bfa62dfda
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-13 01:31:26 +0000

    revert model init file back to inherited code

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-from os import environ as env
+"""
+initialize the models package
+"""
+
 from os import getenv
 
 
-env = getenv('HBNB_TYPE_STORAGE')
-if env == 'db':
+storage_t = getenv("HBNB_TYPE_STORAGE")
+
+if storage_t == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
-    storage.reload()
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-    storage.reload()
+storage.reload()
